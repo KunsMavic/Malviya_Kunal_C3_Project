fb7da387b15a56f06ed801c50d75ebb50a896a53
KunsMavic <88300286+KunsMavic@users.noreply.github.com>
2021-09-29 20:10:36 +0530

    Part 2: Solution
    Part 2: Solution

---
+++ src/main/java/Restaurant.java
@@ -0,0 +1,73 @@
+//import jdk.vm.ci.meta.Local;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Restaurant {
+    private String name;
+    private String location;
+    public LocalTime openingTime;
+    public LocalTime closingTime;
+    private List<Item> menu = new ArrayList<Item>();
+
+    public Restaurant(String name, String location, LocalTime openingTime, LocalTime closingTime) {
+        this.name = name;
+        this.location = location;
+        this.openingTime = openingTime;
+        this.closingTime = closingTime;
+    }
+
+    public boolean isRestaurantOpen() {
+        LocalTime CurrentTime = getCurrentTime(); // declaring object to fetch current time (LocalTime.now())
+        if ((CurrentTime.isAfter(openingTime) || CurrentTime.equals(openingTime)) && (CurrentTime.isBefore(closingTime) || CurrentTime.equals(closingTime))) { //checks if current time falls between restaurant opening and closing time
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public LocalTime getCurrentTime(){ return  LocalTime.now(); }
+
+    public List<Item> getMenu() {
+
+        return menu;//Returns the menu list
+    }
+
+    private Item findItemByName(String itemName){
+        for(Item item: menu) {
+            if(item.getName().equals(itemName))
+                return item;
+        }
+        return null;
+    }
+
+    public void addToMenu(String name, int price) {
+        Item newItem = new Item(name,price);
+        menu.add(newItem);
+    }
+    
+    public void removeFromMenu(String itemName) throws itemNotFoundException {
+
+        Item itemToBeRemoved = findItemByName(itemName);
+        if (itemToBeRemoved == null)
+            throw new itemNotFoundException(itemName);
+
+        menu.remove(itemToBeRemoved);
+    }
+    public void displayDetails(){
+        System.out.println("Restaurant:"+ name + "\n"
+                +"Location:"+ location + "\n"
+                +"Opening time:"+ openingTime +"\n"
+                +"Closing time:"+ closingTime +"\n"
+                +"Menu:"+"\n"+getMenu());
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
---
+++ src/main/java/RestaurantService.java
@@ -0,0 +1,35 @@
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RestaurantService {
+    private static List<Restaurant> restaurants = new ArrayList<>();
+
+    public Restaurant findRestaurantByName(String restaurantName)throws restaurantNotFoundException{
+        for (Restaurant restaurant : restaurants){ //Iterates through the restaurants string present in the list
+
+            if (restaurant.getName().equals(restaurantName)){// checks for restaurant matching input string
+                return restaurant;// returns matching restaurant
+
+            }
+        }
+        throw new restaurantNotFoundException(restaurantName);// throws not found exception if restaurant is not present
+    }
+
+
+    public Restaurant addRestaurant(String name, String location, LocalTime openingTime, LocalTime closingTime) {
+        Restaurant newRestaurant = new Restaurant(name, location, openingTime, closingTime);
+        restaurants.add(newRestaurant);
+        return newRestaurant;
+    }
+
+    public Restaurant removeRestaurant(String restaurantName) throws restaurantNotFoundException {
+        Restaurant restaurantToBeRemoved = findRestaurantByName(restaurantName);
+        restaurants.remove(restaurantToBeRemoved);
+        return restaurantToBeRemoved;
+    }
+
+    public List<Restaurant> getRestaurants() {
+        return restaurants;
+    }
+}
